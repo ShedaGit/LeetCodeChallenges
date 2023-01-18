5efe3d77e9472a86f0fb037a83830450ee7fd478
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-01-18 12:29:56 +0300

    challenge: add MaximumWealth method

--- RichestCustomerWealth/Program.cs
+++ RichestCustomerWealth/Program.cs
@@ -6,4 +6,23 @@ class Program
     {
         Console.WriteLine("Hello World!");
     }
+
+    static public int MaximumWealth(int[][] accounts)
+    {
+        int maxWealth = 0;
+
+        for (int customer = 0; customer < accounts.Length; customer++)
+        {
+            int currentCustomerWealth = 0;
+
+            currentCustomerWealth = accounts[customer].Sum();
+            
+            if (maxWealth < currentCustomerWealth)
+            {
+                maxWealth = currentCustomerWealth;
+            }
+        }
+
+        return maxWealth;
+    }
 }
