bd7871082d727fdea7b3ba36d2ab41890a8244d2
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-01-19 12:18:37 +0300

    challenge: add NumberOfSteps method

--- ReduseToZero/Program.cs
+++ ReduseToZero/Program.cs
@@ -5,4 +5,17 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+
+    static public int NumberOfSteps(int number)
+    {
+        int steps = 0;
+
+        while (number != 0)
+        {
+            number = number % 2 == 1 ? number - 1 : number / 2;
+            steps++;
+        }
+        
+        return steps;
+    }
 }
