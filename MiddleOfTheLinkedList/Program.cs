bceb1b3235b583c39555c08a9e817f997673aee2
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-01-20 16:15:36 +0300

    challenge: add solution for problem 876

--- MiddleOfTheLinkedList/Program.cs
+++ MiddleOfTheLinkedList/Program.cs
@@ -5,4 +5,40 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+
+    public static ListNode MiddleNode(ListNode head)
+    {
+        if (head.next == null)
+        {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null)
+        {
+            if (fast.next.next == null)
+            {
+                slow = slow.next;
+                break;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
 }
