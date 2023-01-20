ccdbdb2b3f855a5b892ae70e75f87cd3523a5db8
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-01-20 14:11:38 +0300

    challenge: add KWeakestRows method for problem 1337

--- TheKWeakestRows/Program.cs
+++ TheKWeakestRows/Program.cs
@@ -4,4 +4,37 @@
     {
         System.Console.WriteLine("Hi there!");
     }
+
+    public static int[] KWeakestRows(int[][] mat, int k)
+    {
+        int[] result = new int[k];
+
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < mat.Length; i++)
+        {
+            int numberOfSoldiers = 0;
+
+            for (int j = 0; j < mat[i].Length; j++)
+            {
+                if (mat[i][j] == 0)
+                {
+                    continue;
+                }
+
+                numberOfSoldiers++;
+            }
+
+            dict[i] = numberOfSoldiers;
+        }
+
+        var sortedDict = dict.OrderBy(x => x.Value).ToList();
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = sortedDict[i].Key;
+        }
+
+        return result;
+    }
 }
